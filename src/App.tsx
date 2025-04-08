b2b0e963789c0eaf6c4ee9597558062df11d1a35
Higor Felype <higor.carvalho@portoseguro.com.br>
2025-04-08 15:19:45 -0300

    Add lucide-react dependency, implement Button component with styles, and enhance App layout with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import styles from "./App.module.scss";
-
+import { Button } from "./components/Button/Button";
 function App() {
   return (
     <div className={styles.container}>
       <h1>Welcome to Your Blog</h1>
       <h2>Recent Posts</h2>
+
+      <div className={styles.buttons}>
+        <Button variant="primary">Apply filters</Button>
+
+        <Button leftIcon="arrow-left" variant="secondary">
+          Back
+        </Button>
+      </div>
     </div>
   );
 }
