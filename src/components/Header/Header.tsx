6b5425a5a5e8ce792153b94b7260877291e1f518
Higor Felype <higor.carvalho@portoseguro.com.br>
2025-04-10 12:31:40 -0300

    Enhance Header component search functionality: implement handleSearch function to manage search input and navigation, updating onSubmit behavior in SearchBar for improved user experience.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,19 @@ import { SearchButton } from "../buttons";
 import { SearchBar } from "../SearchBar";
 import { useApp } from "../../contexts/AppContext";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { setSearch } = useApp();
+  const navigate = useNavigate();
   const { isMobile, isDesktop } = useResponsive();
 
   const [localSearch, setLocalSearch] = useState("");
+
+  const handleSearch = () => {
+    setSearch(localSearch);
+    navigate("/");
+  };
 
   return (
     <header className={styles.header}>
@@ -21,7 +28,7 @@ const Header = () => {
             value={localSearch}
             placeholder="Search"
             onChange={setLocalSearch}
-            onSubmit={() => setSearch(localSearch)}
+            onSubmit={handleSearch}
           />
         )}
         {isMobile && <SearchButton onClick={() => {}} />}
