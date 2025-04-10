4a01dbb105cbe7a11fa5a0656de8b5dae45847f7
Higor Felype <higor.carvalho@portoseguro.com.br>
2025-04-10 03:16:57 -0300

    Add mock data and update PostContent component: introduce mockPosts and mockPost for testing, enhance layout and styling in PostContent with responsive design elements.

--- src/pages/PostDetailsPage/components/PostContent/PostContent.tsx
+++ src/pages/PostDetailsPage/components/PostContent/PostContent.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./PostContent.module.scss";
+import { mockPost } from "../../mocks";
 
 const PostContent: React.FC = () => {
-  return <div className={styles.postContent}>{/* PostContent content */}</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{mockPost.title}</h1>
+      <div className={styles.authorInfo}>
+        <img
+          alt="Author"
+          className={styles.authorImage}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKC0e4V59rDNR5lA5x7-PTmBnDUzlfEwXkiw&s"
+        />
+        <div>
+          <p className={styles.authorName}>
+            <span>Written by:</span> {mockPost.author}
+          </p>
+          <p className={styles.date}>{mockPost.date}</p>
+        </div>
+      </div>
+      <img className={styles.mainImage} src={mockPost.imageUrl} alt="Main" />
+      <div className={styles.text}>
+        {mockPost.description.split("\n").map((line, index) => (
+          <p key={index}>{line}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PostContent;
