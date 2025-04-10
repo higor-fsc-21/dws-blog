145d258b7a713d4f04f2ccd43b5fd1329b63c100
Higor Felype <higor.carvalho@portoseguro.com.br>
2025-04-09 23:40:29 -0300

    Refactor PageHeader component: update styles to use mixins and variables for improved consistency, and restructure layout to include title and SortButton for enhanced functionality.

--- src/pages/HomePage/components/PageHeader/PageHeader.tsx
+++ src/pages/HomePage/components/PageHeader/PageHeader.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./PageHeader.module.scss";
+import { SortButton } from "../../../../components/buttons";
 
 const PageHeader: React.FC = () => {
-  return <div className={styles.pageHeader}>{/* PageHeader content */}</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>DWS blog</h1>
+
+      <SortButton />
+    </div>
+  );
 };
 
 export default PageHeader;
